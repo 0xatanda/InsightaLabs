a05d7b272b978986bf2e6d59bd4dc3599f4159f0
0xatanda <atanda0x@gmail.com>
2026-04-24 11:54:04 +0100

    fixed: refactored

--- internal/config/db.go
+++ internal/config/db.go
@@ -3,22 +3,30 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectDB() *sql.DB {
-	db, err := sql.Open("postgres",
-		"postgres://appuser:password@localhost:5432/profiles_db?sslmode=disable",
-	)
-	if err != nil {
-		log.Fatal("DB connection error:", err)
+
+	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
 	}
 
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("DB open error:", err)
+	}
+
+	// verify connection
 	if err := db.Ping(); err != nil {
 		log.Fatal("DB ping error:", err)
 	}
 
+	// optional but recommended
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(5)
 
