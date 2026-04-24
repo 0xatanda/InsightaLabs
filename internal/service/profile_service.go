7ecf3869088801bfe8c3e7318b5feb0e861a3a09
0xatanda <atanda0x@gmail.com>
2026-04-24 17:43:02 +0100

    fixed: profile

--- internal/service/profile_service.go
+++ internal/service/profile_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/0xatanda/InsightaLabs/internal/domain"
 	"github.com/0xatanda/InsightaLabs/internal/dto"
 	"github.com/0xatanda/InsightaLabs/internal/query"
@@ -13,19 +15,39 @@ type ProfileService struct {
 }
 
 func NewProfileService(r *repository.ProfileRepository, b *query.Builder) *ProfileService {
-	return &ProfileService{r, b}
+	return &ProfileService{
+		repo:    r,
+		builder: b,
+	}
 }
 
 func (s *ProfileService) Get(q dto.ProfileQuery) ([]domain.Profile, int, error) {
 
-	sql, args, countSQL, cargs := s.builder.Build(q)
+	// ✅ VALIDATE sort_by
+	validSort := map[string]bool{
+		"age":                true,
+		"created_at":         true,
+		"gender_probability": true,
+		"":                   true, // allow empty → default
+	}
 
-	data, err := s.repo.Fetch(sql, args)
+	if !validSort[q.SortBy] {
+		return nil, 0, fmt.Errorf("Invalid query parameters")
+	}
+
+	// ✅ VALIDATE order
+	if q.Order != "" && q.Order != "asc" && q.Order != "desc" {
+		return nil, 0, fmt.Errorf("Invalid query parameters")
+	}
+
+	query, args, countQuery, countArgs := s.builder.Build(q)
+
+	data, err := s.repo.Fetch(query, args)
 	if err != nil {
 		return nil, 0, err
 	}
 
-	total, err := s.repo.Count(countSQL, cargs)
+	total, err := s.repo.Count(countQuery, countArgs)
 	if err != nil {
 		return nil, 0, err
 	}
